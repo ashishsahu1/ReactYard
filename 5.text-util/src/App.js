e75e62088d36133671e1bd1480dfa30657c10aab
Ashish Sahu <ashishsahu10428@gmail.com>
2022-01-23 23:36:24 +0530

    state added

--- 5.text-util/src/App.js
+++ 5.text-util/src/App.js
@@ -4,7 +4,7 @@ import TextFrom from './Components/TextFrom';
 
 function App() {
   return (
-    <div classNameName="App">
+    <div className="App">
       <Navbar logo="TextUtils"/>
       <TextFrom heading ="Input Text Area" txt ="Write your text here..."/>
     </div>
--- 5.text-util/src/Components/TextFrom.js
+++ 5.text-util/src/Components/TextFrom.js
@@ -1,14 +1,33 @@
-import React from 'react';
-import PropTypes from 'prop-types'
-
+import React,{useState} from 'react';
+import PropTypes from 'prop-types';
 export default function TextFrom(props) {
+
+    const handleUpClick = ()=>{
+        // console.log("Upper case button clicked "+text);
+        let newTxt = text.toUpperCase();
+        setText(newTxt);
+    }
+
+    const handleLowClick = ()=>{
+        // console.log("Lower case button clicked "+text);
+        let newTxt = text.toLowerCase();
+        setText(newTxt);
+
+    }
+
+    //this add the typing functionality
+    const handleOnChange = (event)=>{
+        setText(event.target.value)
+    }
+    const [text, setText] = useState('Enter something');
     return (
-        <div className='container'>
+        <div >
             <h1>{props.heading}</h1>
-            <div className="mb-">
-                <label for="textArea" className="form-label">{props.txt}</label>
-                <textarea className="form-control" id="textArea" rows="3"></textarea>
-            </div>
+            <div className="mb-3 container">
+                <textarea className="form-control" onChange={handleOnChange} value={text} id="textArea" rows="8"></textarea>
+            </div> 
+            <button className="btn btn-primary my-3 mx-3" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button className="btn btn-primary my-3 mx-3" onClick={handleLowClick}>Convert to Lowercase</button>
         </div>
     )
 }
