458c71087736272d67656d1ed3fbaf47bf493916
Ashish Sahu <ashishsahu10428@gmail.com>
2022-10-05 21:45:52 +0530

    Updates

---
+++ clickToOpen/open-up/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+import PopUp from './PopUp';
+
+function App() {
+
+  return (
+    <div className="App">
+      <PopUp></PopUp>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ clickToOpen/open-up/src/PopUp.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './PopUp.css'
+
+function PopUp() {
+  return (
+    <div className='pop-up'></div>
+  )
+}
+
+export default PopUp
