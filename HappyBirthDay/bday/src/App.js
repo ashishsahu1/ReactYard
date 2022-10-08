30f841269ac1af1e2529c494a7955455adba3cf9
Ashish Sahu <ashishsahu10428@gmail.com>
2022-10-08 12:10:50 +0530

    Update

---
+++ HappyBirthDay/bday/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Start from './Pages/Start';
+
+function App() {
+  return (
+    <div className="App">
+      <Start/>
+    </div>
+  );
+}
+
+export default App;
---
+++ HappyBirthDay/bday/src/Pages/Start.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../Pages/Start.css'
+
+function Start() {
+  return (
+    <div className='cntr'>
+        <div className="cntr-dv">
+            <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci odio possimus reiciendis quis ad fugiat odit architecto iste error impedit sunt officia recusandae itaque et non doloremque, quod, quos ducimus.</h1>
+            <button>CLICK HERE</button>
+        </div>
+    </div>
+  )
+}
+
+export default Start
